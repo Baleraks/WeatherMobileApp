0985be8278c9ea943a42b6c4ff9b2e8cd0f93782
Baleraks <98695234+Baleraks@users.noreply.github.com>
2023-06-19 15:22:45 +0300

    Main page

--- WeatherApp/WeatherApp/Helpers/ApiHelper.cs
+++ WeatherApp/WeatherApp/Helpers/ApiHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace WeatherApp.Helpers
 {
-    internal class ApiHelper
+    public static class ApiHelper
     {
+        public static async Task<Root> GetWeather(double latitude, double longitude)
+        {
+            var httpClient = new HttpClient();
+            var responce = await httpClient.GetStringAsync(String.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid=4280e2f87f6da27bf8dc498af65fa883",latitude,longitude));
+            return JsonConvert.DeserializeObject<Root>(responce);
+        }
+
+
+        public static async Task<Root> GetWeatherByCity(string city)   
+        {
+            var httpClient = new HttpClient();
+            var responce = await httpClient.GetStringAsync(String.Format("https://api.openweathermap.org/data/2.5/forecast?q={0}&units=metric&appid=4280e2f87f6da27bf8dc498af65fa883", city));
+            return JsonConvert.DeserializeObject<Root>(responce);
+        }
+
+
+
+
     }
 }
--- WeatherApp/WeatherApp/View/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/View/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace WeatherApp;
+﻿using WeatherApp.Helpers;
+
+namespace WeatherApp;
 
 public partial class MainPage : ContentPage
 {
@@ -8,6 +10,17 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        var result = await ApiHelper.GetWeather(53.6884000, 23.8258000);
+        lbCity.Text =  result.city.name;
+        lbWeatherDescription.Text = result.list[0].weather[0].description;
+        lbTemperature.Text = result.list[0].main.temperature + "°C";
+        lbHumidity.Text = result.list[0].main.humidity + "%";
+        lbWind.Text = result.list[0].wind.speed + "km/h";
+    }
 
 
 }
